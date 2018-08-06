4790ab51ad4220fa5a170404ba1467cf6dbf563f
DuParkLinux <dupark3@gmail.com>
2018-08-06 15:47:30 -0400

    Added function to check if a phrase in a line of the menu is in the comprehensive beer list

--- pictureToText.py
+++ pictureToText.py
@@ -3,11 +3,31 @@
 from PIL import Image
 import pytesseract, sys
 
-def pictureToText(imageName):
-    return pareseMenuText(pytesseract.image_to_string(Image.open(imageName)))
+def extractBeerNamesFromImg(imageName):
+    return parseMenuText(pytesseract.image_to_string(Image.open(imageName)))
 
 def parseMenuText(menuText):
     menuBeerNames = []
+    allBeerNames = { }
+    beerfile = open('beernames.txt', 'r')
+    for line in beerfile.readlines():
+        allBeerNames[line[:-1]] = 1
+    print('all beer count: ' + str(len(allBeerNames)))
+
+    for line in menuText.split('\n'):
+        lineList = line.split()
+        for i in range(len(lineList)):
+            phrase = ' '.join(lineList[:i])
+            if phrase in allBeerNames:
+                menuBeerNames.append(phrase)
+                print('phrase added: ' + phrase)
+    
+    print('total added : ' + str(len(menuBeerNames)))
+    beerfile.close()
+    return menuBeerNames
+
+# extractBeerNamesFromImg('exampleMenu.jpg')
 
 if len(sys.argv) > 1:
-    print(pytesseract.image_to_string(Image.open(sys.argv[1])))
+    filename = sys.argv[1]
+    print(pytesseract.image_to_string(Image.open(filename)))
