622f0d4f2177bc05658f5597d2b599057ba6cc52
DuParkLinux <dupark3@gmail.com>
2018-08-07 14:59:17 -0400

    Gather upto 50 top beers from user for the purposes of increasing similiarity index. Only gather similar users from upto 5 top beers. Added check for number of checkins on a particular beer being fewer than 25.

--- accessUntappd.py
+++ accessUntappd.py
@@ -9,17 +9,19 @@ client_id = '97759B6FEFAB5D2DCF9398B0067E03B26E7F2C1A'
 client_secret = 'FE8A3493C40B95E16930B07528FF46CCE07954F3'
 baseURL = 'https://api.untappd.com/v4/'
 
+
+# TODO : Store user's top beers in a database, update only if too old
 def getUsersTopBeers(user):
     # Get user's JSON data on the top highly rated beers
     res = requests.get(baseURL + 
-        '/user/beers/%s?client_id=%s&client_secret=%s&sort=highest_rated_you&limit=5' 
+        '/user/beers/%s?client_id=%s&client_secret=%s&sort=highest_rated_you&limit=50' 
         %(user, client_id, client_secret))
     userBeerData = res.json()['response']['beers']['items']
 
-    # Parse through userBeerData and obtain top 5 beer IDs
+    # Parse through userBeerData and obtain top 50 beer IDs
     ### Could change this to top 10% beers?
     usersTopBeers = []
-    for i in range(max(5, len(userBeerData))):
+    for i in range(max(50, len(userBeerData))):
         usersTopBeers.append(userBeerData[i]['beer']['bid'])    
 
     return usersTopBeers
@@ -27,59 +29,65 @@ def getUsersTopBeers(user):
     
 def getSimilarUsers(usersTopBeers):
     # Get users who also rated highly the usersTopBeers
+    topBeersToConsider = 5
     similarUsers = {}
     APIRequestsMade = 0
-    for i in range(len(usersTopBeers)):
+    for i in range(min(topBeersToConsider, len(usersTopBeers))):
         # get beer activity feed of usersTopBeers[i] 
         maxId = ''
         similarUsersThisBeer = []
         # beer #0 gets 5 similar users, #1 gets 4 similar users, #4 gets 1
-        while (len(similarUsersThisBeer) < len(usersTopBeers) - i) and APIRequestsMade < 10:
-            #https://api.untappd.com/v4//beer/checkins/36834?client_id=97759B6FEFAB5D2DCF9398B0067E03B26E7F2C1A&client_secret=max_id=%s
+        while (len(similarUsersThisBeer) < topBeersToConsider - i) and APIRequestsMade < 10:
             res = requests.get(baseURL + 
                 '/beer/checkins/%s?client_id=%s&client_secret=%s&max_id=%s' 
                 %(usersTopBeers[i], client_id, client_secret, maxId))
             beerActivityData = res.json()['response']['checkins']['items']
+            totalActivity = int(res.json()['response']['checkins']['count'])
             APIRequestsMade += 1
-            for j in range(25):
+            for j in range(totalActivity):
                 if beerActivityData[j]['rating_score'] >= 3.75:
                     # get [user_name] and add him to similarUsers
                     similarUser = beerActivityData[j]['user']['user_name']
                     if similarUser not in similarUsers:
                         similarUsersThisBeer.append(similarUser)
-                        similarUsers[similarUser] = 1
+                        similarUsers[similarUser] = []
 
                 if len(similarUsersThisBeer) == len(usersTopBeers) - i:
                     break
             # obtain checkin_id of the 25th activity, pass it again as max_id
-            maxId = beerActivityData[24]['checkin_id']
+            # OR, if the beer is rare enough that it has fewer than 25 checkins, move on
+            if totalActivity < 25:
+                break
+            else:
+                maxId = beerActivityData[24]['checkin_id']
             # loop through the next 25, so forth, until we get the correct number of similar users
     return similarUsers
 
 def updateSimilarityIndex(similarUsers, usersTopBeers):
     # Store similarUsers' similarity index (number of beers matched)
-    for user, similarityIndex in similarUsers:
-        # First, check all beers for ones rated >=4 from similar user
-        similarUsersTopBeers = []
-        offset = 0
-        reachedEndOfBeers = False 
-        while not reachedEndOfBeers or offset > 150:
-            res = requests.get(baseURL + 
-                '/user/beers/%s?client_id=%s&client_secret=%s&limit=50&offset=%s&sort=highest_rated_you'
-                %(user, client_id, client_secret, str(offset)))
-            similarUsersBeerData = res.json()
-            count = int(similarUsersBeerData['beers']['count'])
-            if count != 0:
-                offset += count
-                for j in range(count):
-                    checkinData = similarUsersBeerData['beers']['items'][j]
-                    if checkinData['rating_score'] >= 4:
-                        similarUsersTopBeers.append(checkinData['beer']['bid'])
-                    else:
-                        reachedEndOfBeers = True # what a sad statement
+    APIRequestsMade = 0
+    for user, similarityIndex in similarUsers.items():
+        # First, check top 50 beers for ones rated >=4 from similar user
+        res = requests.get(baseURL + 
+            '/user/beers/%s?client_id=%s&client_secret=%s&limit=50&sort=highest_rated_you'
+            %(user, client_id, client_secret))
+        APIRequestsMade += 1
+        similarUsersBeerData = res.json()['response']['beers']['items']
+
+        for j in range(len(similarUsersBeerData)):
+            if similarUsersBeerData[j]['rating_score'] >= 3.75:
+                similarUsers[user].append(similarUsersBeerData[j]['beer']['bid'])
+            else:
+                break
 
         # Find common beers and update the value in the similarUsers dict
-        similarUsers[key] = len(list(set(usersTopBeers).intersection(similarUsersTopBeers)))
+        similarityIndex = len(list(set(usersTopBeers).intersection(similarUsers[user])))
+        print('user beers : ', end='')
+        print(usersTopBeers)
+        print('similar user ' + user + ' top beers: ', end='')
+        print(similarUsers[user])
+        print('user : ' + user + ' similarity index: ' +str(similarityIndex))
+    print('API Requests made : ' + str(APIRequestsMade))
     
     # End of for loop, move on to next similarUser
 
