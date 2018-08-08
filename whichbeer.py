83a2528184b9f4066973970c08ed1ecabe6de19a
DuParkLinux <dupark3@gmail.com>
2018-08-08 15:47:48 -0400

    Final step complete, checking if any of the menu beers are in the similar user's top 50.

--- whichbeer.py
+++ whichbeer.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python3.5
 
 import accessUntappd, pictureToText, calculateScore
-import sys, pprint, datetime, database
+import sys, pprint, datetime
+import database, beerIDs
 
 def main():
     if len(sys.argv) < 2:
@@ -10,49 +11,59 @@ def main():
 
     username = sys.argv[1]
     pictureFile = sys.argv[2]
-    # currentTime = datetime.datetime.today()
-    
+
+    # Open database file to write to if more than 1 day old        
     now = datetime.datetime.today()
-    if now - database.timeStamp < datetime.timedelta(days=1):
+    if now - database.timeStamp > datetime.timedelta(days=1):
         databaseFile = open('database.py', 'w')
         databaseFile.write('import datetime\n')
         databaseFile.write('timeStamp = ' + pprint.pformat(now) + '\n')
         accessAPI = True
     else:
         accessAPI = False
-    # Find upto 50 top rated beers by user
     print(accessAPI)
 
+    # Find upto 50 top rated beers by user
     if accessAPI:
         usersTopBeers = accessUntappd.getUsersTopBeers(username)
         databaseFile.write('usersTopBeers = ' + pprint.pformat(usersTopBeers, width=1000000) + '\n')
     else:
         usersTopBeers = database.usersTopBeers
-    print(usersTopBeers)
     
     # Find up to 15 other users who also rated the top beers highly
-    # similarUsers = {'Anton_W': [], 'Bordwickc': [], 'Brewbuddy313': [], 'Georgiecat': [], 'Jgrimes12': [], 'JorgesThemeSwimming': [], 'Serendipity81': [], 'Sherman14': [], 'carbomb16': [], 'jjkick123': [], 'knutemilk1': [], 'nwa_beer': [], 'samuelmarciano': [], 'timahrens': [], 'trancechemist': []}
+    # Also find their top beers and save in dict
     if accessAPI:
         similarUsers = accessUntappd.getSimilarUsers(usersTopBeers, username)
+        accessUntappd.getSimilarUsersTopBeers(similarUsers, usersTopBeers)
         databaseFile.write('similarUsers = ' + pprint.pformat(similarUsers, width=1000000) + '\n')
     else:
         similarUsers = database.similarUsers
-    pprint.pprint(similarUsers)
-    sys.exit()
-
-    # Find how many top 10 beers of user these similar users also rated highly
-    accessUntappd.updateSimilarityIndex(similarUsers, usersTopBeers)
-    print('similarity index updated:')
-    pprint.pprint(similarUsers)
 
     # Obtain list of beer names from menu picture
     menuBeerList = pictureToText.extractBeerNamesFromImg(pictureFile)
+    print(menuBeerList)
 
-    # Convert list of menu beers into beer IDs
-    menuBeers = accessUntappd.getBeerIds(menuBeerList)
+    # Convert list of menu beers into a dict to get beer IDs
+    if accessAPI:
+        menuBeerIDs = accessUntappd.getBeerIds(menuBeerList)
+    else:
+        menuBeerIDs = beerIDs.beerDict
+    print(menuBeerIDs)
 
     # Go through the menu beers and update its score
-
+    menuBeerRating = {}
+    for user, similarUserTopBeers in similarUsers.items():
+        for beer, bid in menuBeerIDs.items():
+            if bid in similarUserTopBeers:
+                similarityIndex = len(list(set(similarUserTopBeers).intersection(beerIDs.beerIDList)))
+                if beer in menuBeerRating.keys():
+                    menuBeerRating[beer] += similarityIndex
+                else:
+                    menuBeerRating[beer] = similarityIndex
+                print(beer + ' - ' + user + ' - ' + str(similarityIndex))
+    
+    print('\n\n')
+    print(menuBeerRating)
 
 if __name__=='__main__':
     main()
